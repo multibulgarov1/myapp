3fa1795671594843c0eb5aff7d2b72e9242f7644
multibulgarov1 <multibulgarov1@gmail.com>
2026-06-10 23:37:33 +0300

    add api endpoints

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return "🔥 My First Auto Deploy Flask App"
 
+@app.route("/api")
+def api():
+    return {"status": "ok"}
+
+@app.route("/api/user")
+def user():
+    return {
+        "name": "Pavel",
+        "level": "beginner"
+    }
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
