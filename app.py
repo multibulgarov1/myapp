d1649e80dacd6f2f3e446bcb4153748e7124e398
multibulgarov1 <multibulgarov1@gmail.com>
2026-06-10 23:51:20 +0300

    add POST login API

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🔥 My First Auto Deploy Flask App"
+    return "🔥 Flask API Running"
 
-@app.route("/api")
-def api():
-    return {"status": "ok"}
-
+# GET
 @app.route("/api/user")
 def user():
     return {
         "name": "Pavel",
         "level": "beginner"
     }
+
+# POST (НОВОЕ)
+@app.route("/api/login", methods=["POST"])
+def login():
+    data = request.json  # получаем данные
+
+    username = data.get("username")
+    password = data.get("password")
+
+    if username == "admin" and password == "1234":
+        return jsonify({
+            "status": "success",
+            "message": "Login OK"
+        })
+
+    return jsonify({
+        "status": "error",
+        "message": "Wrong login"
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
